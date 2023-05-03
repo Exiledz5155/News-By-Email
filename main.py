9b02a92aba8069f6f4e1d26f8d2d9804204b1275
Danny Bui <Dannybui5155@gmail.com>
2023-05-03 18:20:33 -0500

    Implemented send email feature. Fixed None type Title and utf encoding error

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from send_email import send_email
+
 
 api_key = "d41f75527e9248a18e3e112a415d22cf"
 url = "https://newsapi.org/v2/everything?q=tesla" \
@@ -11,8 +13,13 @@ request = requests.get(url)
 # Get a dictionary with the data
 content = request.json()
 
-# Access the article titles and description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+# Create a list to store titles and descriptions
+message = ""
 
+# Access the article titles and description
+for index, article in enumerate(content["articles"]):
+    if article["title"] is not None:
+        message = message + f"Title: {article['title']} \nDesciption: {article['description']} \n\n"
+
+message = message.encode("utf-8")
+send_email(message)
